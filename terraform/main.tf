5c7c06a2b841b65262411423a633020a96ed387a
Shankar M <msshankar240698@gmail.com>
2025-06-13 13:02:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,28 @@ provider "aws" {
 }
 
 # Get the Latest Ubuntu 24.04 AMI
-data "aws_ami" "ubuntu_24_04" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
+#data "aws_ami" "ubuntu_24_04" {
+  #most_recent = true
+  #owners      = ["099720109477"] # Canonical
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
+  #filter {
+    #name   = "name"
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  #}
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  #filter {
+   # name   = "virtualization-type"
+    #values = ["hvm"]
+ # }
+#}
+
+data "aws_ssm_parameter" "ubuntu_24_04" {
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
 # EC2 Instance Configuration
 resource "aws_instance" "acquiflow" {
-  ami                    = data.aws_ami.ubuntu_24_04.id
+  ami                    = data.aws_ssm_parameter.ubuntu_24_04.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id         
   vpc_security_group_ids = [var.security_group_id]
